cb1e18f6502b918758ebe1092509e1bc62202010
klh <paullhungdim@iisc.ac.in>
2025-09-27 15:42:11 +0530

    added repositories

---
+++ graph-service/src/main/java/com/scholarsphere/graphservice/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.scholarsphere.graphservice.repository;
+
+import com.scholarsphere.graphservice.model.Professor;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends Neo4jRepository<Professor, String> {
+    
+}
